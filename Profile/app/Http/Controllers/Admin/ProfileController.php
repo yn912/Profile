1d3957268a26584e2415ebedb0815f850eb1e672
taro <taro@example.com>
2025-05-22 22:32:43 +0000

    「PHP/Laravel 14 投稿データを保存しよう」課題提出

--- Profile/app/Http/Controllers/Admin/ProfileController.php
+++ Profile/app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Profile;
+
 class ProfileController extends Controller
 {
     public function add()
@@ -14,6 +16,29 @@ class ProfileController extends Controller
 
     public function create(Request $request)
     {
+        $this->validate($request, Profile::$rules);
+
+        $profile = new Profile;
+        $form = $request->all();
+
+        // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $profile->image_path = basename($path);
+        } else {
+            $profile->image_path = null;
+        }
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+        // 追記ここまで
+        // admin/profile/createにリダイレクトする
         return redirect('admin/profile/create');
     }
 
